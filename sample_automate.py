df512d854ada2291430f6ccaf1bd0514e3bff22e
Muzamil Hussain <muzamil.hussain01645@gmail.com>
2023-01-06 09:08:42 -0600

    Python Automation _ 1

--- sample_automate.py
+++ sample_automate.py
@@ -1,2 +1,15 @@
+// automate file creation with os ?
 import os
-os.listdir
+from datetime import datetime
+
+timestr = datetime.now().strftime("%Y%m%d-%H%M%S-%f")
+dir_path = os.path.join('C:\\Users\\User\\Desktop', timestr)
+folder = os.mkdir(dir_path)
+
+with open(os.path.join(dir_path, 'B' + timestr), 'w') as file:
+    file.write('Exact time is: ' + timestr)
+
+
+os.path.join(os.path.expanduser('~'), 'Desktop')
+
+
